409657419b41982ea2626ee0c62bbba42dcc3086
Balamurugan Kandan <balamurugan.kandan@ukaea.uk>
2024-04-07 09:17:39 +0100

    exports script updated.

--- scripts/martev2_exports.sh
+++ scripts/martev2_exports.sh
@@ -14,17 +14,16 @@ if [ ! -z "$1" ]; then
 fi
 
 echo "***** Workspace Path : $WS_PATH"
-EXPORTS_FILE_PATH=/etc/profile.d/exports.sh
+EXPORTS_FILE_PATH=/etc/profile.d/martev2_exports.sh
+EPICS_BASE_DIR="${WS_PATH}"/epics-base
 
 exports() {
     echo "export MARTe2_DIR=${WS_PATH}/MARTe2-dev" >> ${EXPORTS_FILE_PATH}
     echo "export MARTe2_Components_DIR=${WS_PATH}/MARTe2-components" >> ${EXPORTS_FILE_PATH}
     echo "export OPEN62541_LIB=${WS_PATH}/open62541/build/bin" >> ${EXPORTS_FILE_PATH}
     echo "export OPEN62541_INCLUDE=${WS_PATH}/open62541/build" >> ${EXPORTS_FILE_PATH}
-    echo "export EPICS_BASE=${WS_PATH}/epics-base" >> ${EXPORTS_FILE_PATH}
-    echo "export EPICSPVA=${WS_PATH}/epics-base" >> ${EXPORTS_FILE_PATH}
-    echo "export EPICSCA=${WS_PATH}/epics-base" >> ${EXPORTS_FILE_PATH}
-    echo "export EPICS_HOST_ARCH=linux-x86_64" >> ${EXPORTS_FILE_PATH}
+    echo "export EPICS_BASE=${EPICS_BASE_DIR}" >> ${EXPORTS_FILE_PATH}
+    echo "export EPICS_HOST_ARCH=$($EPICS_BASE_DIR/startup/EpicsHostArch)" >> ${EXPORTS_FILE_PATH}
     echo "export PATH=$PATH:$EPICS_BASE/bin/$EPICS_HOST_ARCH" >> ${EXPORTS_FILE_PATH}
     echo "export SDN_CORE_INCLUDE_DIR=${WS_PATH}/SDN_1.0.12_nonCCS/src/main/c++/include/" >> ${EXPORTS_FILE_PATH}
     echo "export SDN_CORE_LIBRARY_DIR=${WS_PATH}/SDN_1.0.12_nonCCS/target/lib/" >> ${EXPORTS_FILE_PATH}
@@ -33,9 +32,9 @@ exports() {
 }
 
 if [ -f "$EXPORTS_FILE_PATH" ]; then
-    echo "$EXPORTS_FILE_PATH exists."
+    echo "$EXPORTS_FILE_PATH exists. No action required."
 else
-    echo "$EXPORTS_FILE_PATH does not exist."
+    echo "$EXPORTS_FILE_PATH does not exist. Copying the file"
     exports
 fi
 
